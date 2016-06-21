3af08a225ec7e173b5b6b4054afe781887ea6277
dutertetimes <dutertetimes@gmail.com>
2016-06-22 03:56:45 +0800

    Add bash script to post/tweet news bit(s)

---
+++ newsbit/post.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Tweet messages and commit news bit(s) to github.
+#
+# Usually this is scheduled to run using at.
+# at -f post.sh
+
+../create_post.sh -nb "Duterte admin will set up agricultural laboratories in each region, according to Incoming Agriculture Secretary Emmanuel F. Piñol."
+../create_post.sh -nb "Duterte admin targets to lift 10 million Filipino families out of Poverty Line."
+../create_post.sh -nb "Duterte: Do mining right or get out."
+../create_post.sh -nb "Duterte: All mining contracts will be reviewed."
+../create_post.sh -nb "Comelec en banc accepted Commissioner Christian Robert Lim's resignation as Comelec Campaign Finance Officer."
+../create_post.sh -nb "Sonny Belmonte in favor of granting incoming president Duterte with emergency powers to solve traffic problems."
+../create_post.sh -nb "Gina Lopez accepts Department of Environment and Natural Resources (DENR) position."
+../create_post.sh -nb "Gina Lopez: I believe we can have a country free of poverty if we take care of the environment."
+../create_post.sh -nb "CHR is alarmed with the increasing human rights violations against suspects. CHR creates task force on extra judicial killings."
+../create_post.sh -nb "CHR will support Duterte government's drive versus criminality as long as it is according to law."
+../create_post.sh -nb "PNP confirms head found in Sulu to be Robert Hall's."
