9dae1df7f6f2694277dc9db8d0fd683e8bdc1e6b
dutertetimes <dutertetimes@gmail.com>
2016-07-06 19:09:03 +0800

    Reorganize site

---
+++ news/bits/_posts/post.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Tweet messages and commit news bit(s) to github.
+#
+# Usually this is scheduled to run using at.
+# at -f post.sh now + 1 minute
+#
+# ../../create_post.sh -nb "<message here>"
+
+../../create_post.sh -nb ""
