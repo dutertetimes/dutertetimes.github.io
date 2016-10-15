71514a3a66b2b26e8b16fa957a5aff1dcc01c2ae
dutertetimes <dutertetimes@gmail.com>
2016-10-15 09:33:36 +0800

    Added 'symbol' in the post creation script

--- create_post.sh
+++ create_post.sh
@@ -43,7 +43,7 @@ function add_default {
 function add_empty {
     echo >> $filename
     echo "Malacañan" >> $filename
-    echo "’“”…" >> $filename
+    echo "‘’“”…" >> $filename
     echo "Empty." >> $filename
     echo "&#x25cf;" >> $filename
 }
