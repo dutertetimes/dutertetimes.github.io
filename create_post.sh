602f9dcd936a2e580d35b4985278ceec7a0f09d3
dutertetimes <dutertetimes@gmail.com>
2017-01-16 15:33:06 +0800

    Add categories to post creation script

--- create_post.sh
+++ create_post.sh
@@ -76,7 +76,7 @@ function create_news_post {
         add_default "News"
         echo "excerpt: " >> $filename
         echo "layout: post" >> $filename
-        echo "categories: [ governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
+        echo "categories: [ governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | energy | social_welfare | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
         echo "tags: []" >> $filename
         if [ $# -eq 0 ]; then
             echo "published: true" >> $filename
@@ -121,7 +121,7 @@ function create_story_post {
         add_default "Story"
         echo "excerpt: " >> $filename
         echo "layout: post" >> $filename
-        echo "categories: [ governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
+        echo "categories: [ governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | energy | social_welfare | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
         echo "tags: []" >> $filename
         if [ $# -eq 0 ]; then
             echo "published: true" >> $filename
@@ -303,7 +303,7 @@ function create_newsbit {
 
         add_yaml_bar
         add_default "News Bit"
-        echo "categories: [ newsbit | governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
+        echo "categories: [ newsbit | governance | economy | mining | manufacturing | services | agrarian | agriculture | agrarian reform | education | environment | transport | scitech | foreign_affairs | culture | energy | social_welfare | peace_process | insurgency | cpp | mnlf | milf | senate | law_and_order | military | pnp | pcg | ofw | press ]" >> $filename
         if [ $# -gt 0 ]; then
             echo "published: true" >> $filename
         else
